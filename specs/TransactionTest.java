2c2a8eb8a0c22e3cd3902107157bd1884db18714
Paul Milne <hallhill@gmail.com>
2016-11-14 17:50:59 +0000

    made a transaction method to get the name of the shop

--- specs/TransactionTest.java
+++ specs/TransactionTest.java
@@ -9,14 +9,21 @@ public class TransactionTest {
      Customer customer;
      Shop shop;
      Double amount;
+     Transaction transaction;
 
      @Before
      public void Before(){
 
           customer = new Customer("Zippy");
-
           shop = new Shop("BearsRUrsus");
-
           amount = new Double(500.00);
+          transaction = new Transaction(1, customer, shop, amount);
+     }
+
+     @Test
+     public void canGetShopName(){
+          Shop thisShop = transaction.getShop();
+          String shopName = thisShop.getName();
+          assertEquals("BearsRUrsus", shopName);
      }
 }
