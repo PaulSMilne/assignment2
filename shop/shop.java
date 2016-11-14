60d66632129b42a9e7c8ff2288f60c90047a414f
Paul Milne <hallhill@gmail.com>
2016-11-14 15:57:35 +0000

    Added a Shop class with a name, sales amount, refunds amount, total funds amount and a list of accepted cards.

--- shop/shop.java
+++ shop/shop.java
@@ -3,6 +3,35 @@ import java.util.*;
 
 public class Shop{
 
+     private String name;
+     private double sales;
+     private double refunds;
+     private double totalShopFunds;
+     private ArrayList<CardType> acceptedCards;
 
-     
-}
+     public Shop(String name){
+          this.name = name;
+          this.sales = 0.00;
+          this.refunds = 0.00;
+          this.totalShopFunds = 0.00;
+          this.acceptedCards = new ArrayList<CardType>();
+     }
+
+     public String getName(){
+          return this.name;
+     }
+
+     public double getSales(){
+          return this.sales;
+     }
+
+     public double getRefunds(){
+          return this.refunds;
+     }
+
+     public double getTotalShopFunds(){
+          return this.totalShopFunds;
+     }
+
+
+}    
---
+++ specs/ShopTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+import shop.*;
+//import behaviours.*;
+
+
+public class CustomerTest {
+
+
+
+
+
+}
