8afe447cb3d46a5250f397f8f50205b6602442ad
Paul Milne <hallhill@gmail.com>
2016-11-14 17:01:46 +0000

    started a new transaction class with an id, customer, shop, card, and amount of sale

---
+++ shop/transaction.java
@@ -0,0 +1,18 @@
+package shop;
+import java.util.*;
+
+public class Transaction{
+
+     private int id;
+     private Customer customer;
+     private Shop shop;
+     private Card card;
+     private Double amount;
+
+     public Transaction(int id, Customer customer, Shop shop, Card card, double amount){
+          this.id = name;
+          this.customer = customer;
+          this.shop = shop;
+          this.card = card;
+          this.amount = amount;
+     }
