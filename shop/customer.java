92b60e45580cc0f47b08ce892a1cb9a5144e3d6b
Paul Milne <hallhill@gmail.com>
2016-11-16 11:24:08 +0000

    removed the totalfunds instance variable from the customer class, as the amount is always generated dyamically from the amount on individual payment methods

--- shop/customer.java
+++ shop/customer.java
@@ -4,13 +4,11 @@ import java.util.*;
 public class Customer{
 
      private String name;
-     private double totalFunds;
      private HashMap<BankCard, Double> paymentOptions;
 
      public Customer(String name){
           this.name = name;
           this.paymentOptions = new HashMap<BankCard, Double>();
-          this.totalFunds = 0;
      }
 
      public String getName() {return this.name;}
